2ae0ede60423d5fd4b34433e782ccc6a159aa8a2
sivavoonna2003 <sivavvoonna2003@gmail.com>
2024-02-03 11:58:28 +0530

    First Commit

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 window.addEventListener('scroll',() =>{
     document.querySelector('nav').classList.toggle('window-scroll',window.scrollY > 0)
 })
+// index.js
+var counterContainer = document.querySelector(".website-counter");
+var visitCount = localStorage.getItem("page_view") || 1;
+
+visitCount = Number(visitCount) + 1;
+localStorage.setItem("page_view", visitCount);
+counterContainer.innerHTML = visitCount;
